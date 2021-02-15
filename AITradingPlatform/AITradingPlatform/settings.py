7b9a9aa402397c9891c80217f7220129829ebbca
Varun487 <seshuvarun@gmail.com>
2021-02-15 18:56:36 +0530

    Migrated DB to Postgresql

--- AITradingPlatform/AITradingPlatform/settings.py
+++ AITradingPlatform/AITradingPlatform/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'AITradingPlatform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'capstoneproject',
+        'USER': 'capstoneprojectuser',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
