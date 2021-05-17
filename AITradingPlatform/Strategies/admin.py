fdf44f0fc4d63afafb953d97e624461d2afa8218
DishaCV <51355151+DishaCV@users.noreply.github.com>
2021-05-18 02:16:56 +0530

    Update admin.py
    Added the tables created in the models module.

--- AITradingPlatform/Strategies/admin.py
+++ AITradingPlatform/Strategies/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import ExampleStrategiesModel
+from .models import ExampleStrategiesModel,Company, Strategy, Orders
 
 # Register your models here.
 admin.site.register(ExampleStrategiesModel)
+admin.site.register(Company)
+admin.site.register(Strategy)
+admin.site.register(Orders)
