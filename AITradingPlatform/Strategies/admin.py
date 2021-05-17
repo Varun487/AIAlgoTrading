0a6cf8dabd9bd6533dd62d0a852d901d28d2635c
DishaCV <51355151+DishaCV@users.noreply.github.com>
2021-05-18 02:18:45 +0530

    Completed the creation of the database for Strategies

--- AITradingPlatform/Strategies/admin.py
+++ AITradingPlatform/Strategies/admin.py
@@ -5,4 +5,4 @@ from .models import ExampleStrategiesModel,Company, Strategy, Orders
 admin.site.register(ExampleStrategiesModel)
 admin.site.register(Company)
 admin.site.register(Strategy)
-admin.site.register(Order)
+admin.site.register(Orders)
