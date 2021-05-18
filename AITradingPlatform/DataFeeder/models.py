3aa684a9dfb735febab8a113c6f34319462fd3ed
Varun487 <seshuvarun@gmail.com>
2021-05-18 12:17:23 +0530

    Fixed breaking changes

--- AITradingPlatform/DataFeeder/models.py
+++ AITradingPlatform/DataFeeder/models.py
@@ -48,7 +48,7 @@ class Indicators(models.Model):
     value = models.FloatField(default=0.0)
     column = models.CharField(max_length=20, default='Default')
     indicator_time_period = models.IntegerField(default=0)
-    candle_stick = models.ForeignKey(to=ImmutableData, on_delete=models.DO_NOTHING, blank=True)
+    candle_stick = models.ForeignKey(to=ImmutableData, on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return f"Indicator: {self.name}, {self.candle_stick}"
