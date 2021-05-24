8499651dbb7c8fa2fd0e74273c14fd3e13993c9a
hritik <hritik.24shanbhag@gmail.com>
2021-05-24 22:55:57 +0530

    completed rest api documentation page

--- AITradingPlatform/UI/ai-trading-platform-ui/src/components/RestApi/EndpointsDescriptionList.vue
+++ AITradingPlatform/UI/ai-trading-platform-ui/src/components/RestApi/EndpointsDescriptionList.vue
@@ -418,6 +418,301 @@ export default {
     "error": "No data present that fits all conditions. Please try sourcing the data or computing indicators."
 }`,
         },
+        {
+          type: "POST",
+          code: "/datafeeder/derivedindicatorcalc/",
+          link: "derived-indicator-calc",
+          description:
+            "Derive Indicator values for different time periods by calculating the standard deviation and simple moving average.",
+          parameters: [
+            {
+              name: "company",
+              type: "Array of Strings",
+              desc:
+                "An array of strings mentioning the stock tickers to collect data from",
+            },
+            {
+              name: "start_date",
+              type: "Date time string",
+              desc:
+                "A string in the format of yyyy-mm-dd hh:MM:ss. It denotes the datetime to start collecting data for the list of stock tickers provided above.",
+            },
+            {
+              name: "end_date",
+              type: "Date time string",
+              desc:
+                "A string in the format of yyyy-mm-dd hh:MM:ss. It denotes the datetime to end collecting data for the list of stock tickers provided above.",
+            },
+            {
+              name: "provider",
+              type: "String",
+              desc:
+                "Data provider to source data from. Can only take values of 'Yahoo' and 'Alpha'",
+            },
+            {
+              name: "time_period",
+              type: "String",
+              desc:
+                "Mentions the time period of data to be collected. If sourcing from Yahoo finance, then value has to be 'daily'. Else, if sourcing from Alpha Vantage, can take any of the following values ['1min', '5min', '15min', '30min', '60min']",
+            },
+            {
+              name: "slice",
+              type: "String",
+              desc:
+                "Is applicable only for Alpha vantage, can take values in range('year1month1' to 'year1month12') and range('year2month1' to 'year2month12'). Even though the slice is not required for Yahoo Finance, it must be present and take a valid value for the request to be valid",
+            },
+            {
+              name: "column",
+              type: "Array of Strings",
+              desc:
+                "An array of strings that can take values ['Open','Low','Close','Volume','High']",
+            },
+            {
+              name: "indicator_time_period",
+              type: "Integer",
+              desc:
+                "An integer mentioning the indicator time period",
+            },
+            {
+              name: "name",
+              type: "Array of Strings",
+              desc:
+                "An array of strings  that can take values ['SMA','Std_Dev','BB_down','BB_up']",
+            }
+          ],
+          req_url: "/datafeeder/derivedindicatorcalc/",
+          req_body: `{
+    "company": "TCS.NS",
+    "start_date": "2021-05-1 00:00:00",
+    "end_date": "2021-05-15 00:00:00",
+    "provider": "Yahoo",
+    "time_period": "daily",
+    "slice": "year1month1",
+    "column": "Volume",
+    "indicator_time_period":4,
+    "name":"SMA"
+}`,
+          correct_output: `{
+    "status": "sucess",
+    "validation_status": "sucess",
+    "collected_data": [
+        "TCS.NS"
+    ],
+    "data_not_found": [],
+    "retrieval_status": "sucess",
+    "convert_dataframe_status": "sucess",
+    "calculated_indicators_status": "sucess",
+    "pushed_status": "sucess"
+}`,
+          failed_output: `{
+    "status": "Invalid request, please check the documentation for the correct request format."
+}`,
+        },
+        {
+          type: "POST",
+          code: "/datafeeder/listimmutable/",
+          link: "list-immutable",
+          description:
+            "List all the immutable data that are filtered according to open, high, low, close, volume, company, time period values DONE",
+          parameters: [
+            {
+              name: "company",
+              type: "Array of Strings",
+              desc:
+                "An array of strings mentioning the stock tickers to collect data from",
+            },
+            {
+              name: "start_date",
+              type: "Date time string",
+              desc:
+                "A string in the format of yyyy-mm-dd hh:MM:ss. It denotes the datetime to start collecting data for the list of stock tickers provided above.",
+            },
+            {
+              name: "end_date",
+              type: "Date time string",
+              desc:
+                "A string in the format of yyyy-mm-dd hh:MM:ss. It denotes the datetime to end collecting data for the list of stock tickers provided above.",
+            },
+            
+            {
+              name: "time_period",
+              type: "String",
+              desc:
+                "Mentions the time period of data to be collected. If sourcing from Yahoo finance, then value has to be 'daily'. Else, if sourcing from Alpha Vantage, can take any of the following values ['1min', '5min', '15min', '30min', '60min']",
+            }
+            
+          ],
+          req_url: "/datafeeder/listimmutable/",
+          req_body: `{
+    "company": "TCS.NS",
+    "start_date": "2021-05-1 00:00:00",
+    "end_date": "2021-05-15 00:00:00",
+    "time_period": "daily"
+    
+}`,
+          correct_output: `
+    "validation_status": "sucess",
+    "company": {
+        "name": "tcs nse",
+        "ticker": "TCS.NS",
+        "sector": "tech"
+    },
+    "immutable": [
+        {
+            "open": 3024.89990234375,
+            "low": 3004.0,
+            "high": 3055.0,
+            "close": 3037.0,
+            "volume": 1545831,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-03T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3062.800048828125,
+            "low": 3035.0,
+            "high": 3090.0,
+            "close": 3049.75,
+            "volume": 1990777,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-04T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3070.0,
+            "low": 3052.39990234375,
+            "high": 3099.39990234375,
+            "close": 3095.699951171875,
+            "volume": 1939289,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-05T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3105.5,
+            "low": 3074.0,
+            "high": 3134.0,
+            "close": 3111.449951171875,
+            "volume": 1791671,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-06T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3133.0,
+            "low": 3111.800048828125,
+            "high": 3159.85009765625,
+            "close": 3132.89990234375,
+            "volume": 1912540,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-07T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3145.949951171875,
+            "low": 3125.0,
+            "high": 3164.199951171875,
+            "close": 3145.5,
+            "volume": 2137153,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-10T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3125.0,
+            "low": 3091.14990234375,
+            "high": 3128.10009765625,
+            "close": 3122.60009765625,
+            "volume": 2415625,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-11T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3120.0,
+            "low": 3070.60009765625,
+            "high": 3120.0,
+            "close": 3087.60009765625,
+            "volume": 1978558,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-12T00:00:00Z",
+            "time_period": "daily"
+        },
+        {
+            "open": 3098.5,
+            "low": 3040.0,
+            "high": 3098.5,
+            "close": 3051.5,
+            "volume": 2043935,
+            "company": {
+                "id": 2,
+                "name": "tcs nse",
+                "ticker": "TCS.NS",
+                "sector": "tech",
+                "data_provider": "Yahoo"
+            },
+            "time_stamp": "2021-05-14T00:00:00Z",
+            "time_period": "daily"
+        }
+    ],
+    "filtering status": "sucess"
+}`,
+          failed_output: `{
+    "status": "Invalid request, please check the documentation for the correct request format."
+}`,
+        }
+        
+        
+        
+
       ],
       StrategiesApis: [
         {
@@ -571,6 +866,26 @@ export default {
     "error": "invalid request, please check the documentation for the correct request format"
 }`,
         },
+        {
+          type: "GET",
+          code: "/strategies/getorders/",
+          link: "get-orders",
+          description:
+            "An example api to test the working of the Strategies component of the project.",
+          parameters: [
+            {
+              name: "string",
+              type: "String",
+              desc: "Enter any string in the url above",
+            },
+          ],
+          req_url: "/strategies/getorders/",
+          req_body: ``,
+          correct_output: `{
+    "name": "hello-strategies"
+}`,
+          failed_output: `{}`,
+        }
       ],
       BackTesterApis: [
         {
