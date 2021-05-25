5d844c06fb50bddb37f99919a7a35c9bf91cd921
hritik <hritik.24shanbhag@gmail.com>
2021-05-25 12:30:10 +0530

    merge change

--- AITradingPlatform/BackTester/views.py
+++ AITradingPlatform/BackTester/views.py
@@ -16,3 +16,8 @@ def api_index(req, slug):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     return Response(ExampleBackTesterSerializer(name).data)
+
+
+@api_view(['POST', ])
+def api_get_orders(req):
+
