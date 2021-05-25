e750649ccc84255fbb092d26c504dd9d9afd5f2c
SamrudhiRRao <raosamrudhi@gmail.com>
2021-05-25 12:35:00 +0530

    Updating the work done in Backtester

--- AITradingPlatform/BackTester/urls.py
+++ AITradingPlatform/BackTester/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('generatereport/', views.api_generate_report, name='api_generate_report'),
     path('<slug>/', views.api_index, name='api_index')
 ]
--- AITradingPlatform/BackTester/views.py
+++ AITradingPlatform/BackTester/views.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -16,3 +18,8 @@ def api_index(req, slug):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     return Response(ExampleBackTesterSerializer(name).data)
+
+@api_view(['POST', ])
+def api_generate_report(req):
+    req_body = json.loads(req.body)
+
