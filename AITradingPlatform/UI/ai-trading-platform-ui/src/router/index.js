a187825fea5cdb75325a4ff61fea9d7daeb1509b
Varun487 <seshuvarun@gmail.com>
2021-05-29 13:09:09 +0530

    Remove home and about pages

--- AITradingPlatform/UI/ai-trading-platform-ui/src/router/index.js
+++ AITradingPlatform/UI/ai-trading-platform-ui/src/router/index.js
@@ -6,25 +6,25 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
+    name: 'APIDocs',
+    component: () => import('../views/RestApiDocs.vue'),
   },
-  {
-    path: '/home',
-    name: 'Home',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-	component: () => import('../views/Home.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue'),
-  },
+//   {
+//     path: '/home',
+//     name: 'Home',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+// 	component: () => import('../views/Home.vue'),
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import('../views/About.vue'),
+//   },
   {
 	path: '/api',
     name: 'APIDocs',
