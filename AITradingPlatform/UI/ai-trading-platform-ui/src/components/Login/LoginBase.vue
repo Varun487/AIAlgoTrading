bbb0d5a1f1126bca68b7855a92f626d9a600b3c2
Varun487 <seshuvarun@gmail.com>
2021-05-30 13:29:49 +0530

    merge commit

--- AITradingPlatform/UI/ai-trading-platform-ui/src/components/Login/LoginBase.vue
+++ AITradingPlatform/UI/ai-trading-platform-ui/src/components/Login/LoginBase.vue
@@ -1,92 +1,84 @@
 <template>
-
-    <div class="login"> 
-    <h1>Login Form</h1> 
-    <br><br>
-    <form id="login" method="get" action="">    
-        <label><b>User Name</b></label>       
-        <input type="text" name="Username" id="Username" >    
-        <br><br>    
-        <label><b>Password</b></label>        
-        <input type="Password" name="Password" id="Password" >    
-        <br><br>    
-        <input type="button" name="log_button" id="log_button" value="Log In" @click="login">       
-        <br><br>    
-        <input type="checkbox" id="check"><span>Remember me</span>     
-    </form>     
-    </div>
-   
+  <div class="login">
+    <h1>Login</h1>
+    <br /><br />
+    <form id="login" method="get" action="">
+      <label><b>User Name</b></label>
+      <input type="text" name="Username" id="Username" />
+      <br /><br />
+      <label><b>Password</b></label>
+      <input type="Password" name="Password" id="Password" />
+      <br /><br />
+      <input
+        type="button"
+        name="log_button"
+        id="log_button"
+        value="Log In"
+        @click="login"
+      />
+      <br /><br />
+      <input type="checkbox" id="check" /><span>Remember me</span>
+    </form>
+  </div>
 </template> 
 
 <script>
 export default {
   name: "LoginBase",
   methods: {
-      login() {
-        
-      },
-    },
-  }
-  
+    login() {},
+  },
+};
 </script>
 
-
-
-
-
-<style scoped> 
-.login{  
-        width: 382px;  
-        overflow: hidden;
-        margin: auto;  
-        margin: 20 300 300 450px;
-        padding: 80px;  
-        background: #000000;  
-        border-radius: 15px ; 
-        top: 0px;
-          
-}  
-h1{  
-    text-align: center;  
-    color: #ffffff;  
-    padding: 20px;  
-}  
-label{  
-    color: yellow;  
-    font-size: 17px;  
-}  
-#Username{  
-    width: 300px;  
-    height: 30px;  
-    border: none;  
-    border-radius: 3px;  
-    padding-left: 8px;  
-}  
-#Password{  
-    width: 300px;  
-    height: 30px;  
-    border: none;  
-    border-radius: 3px;  
-    padding-left: 8px;  
-      
-}  
-#log_button{  
-    width: 300px;  
-    height: 30px;  
-    border: none;  
-    border-radius: 17px;  
-    padding-left: 7px;  
-    color: #000000; 
-  
-  
-}  
-span{  
-    color: white;  
-    font-size: 17px;  
+<style scoped>
+.login {
+  width: 382px;
+  overflow: hidden;
+  margin: auto;
+  padding: 80px;
+  background: #111111;
+  border-radius: 15px;
+  top: 0px;
+  box-shadow: 0px 0px 5px lightblue;
 }
 
+h1 {
+  text-align: center;
+  padding: 20px;
+}
 
-
+label {
+  opacity: 0.8;
+  font-size: 17px;
+  padding-bottom: 20px;
+}
+#Username {
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 3px;
+  padding-left: 8px;
+}
+#Password {
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 3px;
+  padding-left: 8px;
+}
+#log_button {
+  width: 300px;
+  height: 30px;
+  border: none;
+  border-radius: 17px;
+  padding-left: 7px;
+  color: #000000;
+}
+span {
+  color: white;
+  font-size: 17px;
+}
 </style>
 
 
