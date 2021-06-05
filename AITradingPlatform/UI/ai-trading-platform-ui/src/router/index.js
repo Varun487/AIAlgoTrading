d1179d1a40295ba63ad54f964abe209515882dfe
DishaCV <51355151+DishaCV@users.noreply.github.com>
2021-06-05 19:04:27 +0530

    Update index.js

--- AITradingPlatform/UI/ai-trading-platform-ui/src/router/index.js
+++ AITradingPlatform/UI/ai-trading-platform-ui/src/router/index.js
@@ -33,6 +33,14 @@ const routes = [
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Strategies.vue')
+  },
+  {
+    path: '/papertrader',
+      name: 'Papertrader',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Papertrader.vue')
   }
 ]
 
