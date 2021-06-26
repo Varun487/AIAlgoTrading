59c28f4c1dab1d5f8b1832f9cce4fd761ac99925
hriti24k <hritik.24shanbhag@gmail.com>
2021-06-26 12:23:00 +0530

    incomplete restapi

--- restapi/backtester/urls.py
+++ restapi/backtester/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('getbacktests',views.api_listbacktests, name='api_listbacktests'),
+    path('getreports/<id>/',views.api_listreports,name='api_listreports'),
     path('<slug>/', views.api_index, name='api_index')
 ]
