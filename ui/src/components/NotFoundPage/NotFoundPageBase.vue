2040fbee5ab7f71a536549b5210a0a34cbd35c26
hriti24k <hritik.24shanbhag@gmail.com>
2021-08-21 20:37:44 +0530

    merge changes

--- ui/src/components/NotFoundPage/NotFoundPageBase.vue
+++ ui/src/components/NotFoundPage/NotFoundPageBase.vue
@@ -2,7 +2,8 @@
     <div>
         <center>
             <h1>404 not found</h1>
-            <h2>you're in the wrong page</h2>
+            <h3>THe PAGE YOU WERE LOOKING FOR DOES NOT EXIST</h3>
+            <h6>YOU MAY HAVE MISTYPED THE ADDRESS OR THE PAGE MAY HAVE MOVED</h6>
         </center>
     </div>
 </template>
